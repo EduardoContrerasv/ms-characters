096143ac983e95c08de3c357a95c609ba42c79fa
EduardoCV <edu.contrerasv@duocuc.cl>
2026-05-11 22:47:28 -0400

    Creacion ms-character

---
+++ src/test/java/cl/duoc/ms_characters/MsCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_characters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
